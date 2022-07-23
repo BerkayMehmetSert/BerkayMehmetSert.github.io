8d7703acfc5a45f0e2441fd689e2c6ec14c89c80
BerkayMehmetSert <berkaymehmetsert@gmail.com>
2022-07-23 13:58:35 +0300

    Add new projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,27 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: 'https://www.google.com',
+    title: 'Instagram UI Clone with Figma',
+    description: `Responsive Instagram UI Clone with Figma`,
+    imgSrc: '/static/images/projects/instagram-ui-clone.jpg',
+    href: 'https://www.figma.com/community/file/1039921953318471355',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Youtube UI Clone with Figma',
+    description: `Responsive Youtube UI Clone with Figma`,
+    imgSrc: '/static/images/projects/youtube-ui-clone.jpg',
+    href: 'https://www.figma.com/community/file/1039193156346432955',
+  },
+  {
+    title: 'Twitter UI Clone with Figma',
+    description: `Responsive Twitter UI Clone with Figma`,
+    imgSrc: '/static/images/projects/twitter-ui-clone.jpg',
+    href: 'https://www.figma.com/community/file/1039189378542401977',
+  },
+  {
+    title: 'Design System with Figma',
+    description: `Responsive design system with Figma. 350+ components, 50+ colors, 60+ font styles, and more.`,
+    imgSrc: '/static/images/projects/design-system.jpg',
+    href: 'https://www.figma.com/community/file/1073281213390707834',
   },
 ]
 
