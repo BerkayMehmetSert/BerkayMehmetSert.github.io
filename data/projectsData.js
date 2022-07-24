3fe926f82e420b159a435bdcc8970ce17b731bed
BerkayMehmetSert <berkaymehmetsert@gmail.com>
2022-07-24 17:22:45 +0300

    Add new projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -23,6 +23,42 @@ const projectsData = [
     imgSrc: '/static/images/projects/design-system.jpg',
     href: 'https://www.figma.com/community/file/1073281213390707834',
   },
+  {
+    title: 'NFT Marketplace UI Concept',
+    description: `Hello there! I hope you're good. This is an NFT Marketplace Header UI Concept. Made with Figma.`,
+    imgSrc: '/static/images/projects/nft.png',
+    href: 'https://dribbble.com/shots/18877443-NFT-Marketplace-Header',
+  },
+  {
+    title: 'acc® - Furniture Landing Page Design',
+    description: `Hello there! I hope you're good. This is an Furniture Landing Page UI Concept. Made with Figma.`,
+    imgSrc: '/static/images/projects/furniture.png',
+    href: 'https://dribbble.com/shots/18877472-acc-Furniture-Landing-Page-Design',
+  },
+  {
+    title: 'Starbucks® - Website Concept',
+    description: `Hello there! I hope you're good. This is an Starbucks® UI Concept. Made with Figma.`,
+    imgSrc: '/static/images/projects/starbucks.png',
+    href: 'https://dribbble.com/shots/18877557-Starbucks-Website-Concept',
+  },
+  {
+    title: '+1 blog. - Minimal Travel Blog Website Concept',
+    description: `Hello there! I hope you're good. This is an Minimal Travel Blog Website Concept. Made with Figma.`,
+    imgSrc: '/static/images/projects/travel-blog.png',
+    href: 'https://dribbble.com/shots/18877575--1-blog-Minimal-Travel-Blog-Website-Concept',
+  },
+  {
+    title: 'Hash • Digital® - Digital Studio Website Concept',
+    description: `Hello there! I hope you're good. This is an Digital Studio Website Concept. Made with Figma.`,
+    imgSrc: '/static/images/projects/design-studio.png',
+    href: 'https://dribbble.com/shots/18877590-Hash-Digital-Digital-Studio-Website-Concept',
+  },
+  {
+    title: '&lab.House® - E-commerce Website Concept',
+    description: `Hello there! I hope you're good. This is an E-commerce Website Concept. Made with Figma.`,
+    imgSrc: '/static/images/projects/e-commerce.png',
+    href: 'https://dribbble.com/shots/18877605--lab-House-E-commerce-Website-Concept',
+  },
 ]
 
 export default projectsData
